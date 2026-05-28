49eddb72756e377a4e466792df9ee955e2e4b974
Gabriel-da-Silva-Puente <gabrielpuente4848@gmail.com>
2026-05-28 01:18:01 -0300

    Correcao CORS

--- pontorecarga-api/src/main/java/br/com/ABICAP/pontorecarga_api/config/CorsConfig.java
+++ pontorecarga-api/src/main/java/br/com/ABICAP/pontorecarga_api/config/CorsConfig.java
@@ -14,9 +14,13 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500")
+                        .allowedOrigins(
+                                "http://127.0.0.1:5500",
+                                "http://localhost:5500"
+                        )
                         .allowedMethods("*")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
